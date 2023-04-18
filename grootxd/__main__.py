24ce55cb4732e5cfe71fb7dbbff0678ab57108b2
ɢʀᴏᴏᴛ xᴅ <128709493+9505GrootXD@users.noreply.github.com>
2023-04-18 10:49:26 +0530

    Create __main__.py

---
+++ grootxd/__main__.py
@@ -0,0 +1,11 @@
+from grootxd import bot
+from pyrogram import idle
+from grootxd.clientbot import run
+from pyrogram import Client as Bot
+from grootxd.config import API_ID, API_HASH, BOT_TOKEN
+
+
+bot.start()
+run()
+idle()
+print("🌱 𝗚𝗿𝗼𝗼𝘁 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱...👻")
